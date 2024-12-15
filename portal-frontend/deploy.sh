0e6368fd1551c9c6ab364594c19f0aa9c4e87f53
Jaskaran Singh Bhalla <jaskaransinghbhalla.mgsb@gmail.com>
2024-12-15 18:02:10 +0530

    Made some Minor changes on server

--- portal-frontend/deploy.sh
+++
@@ -1,4 +0,0 @@
-rsync \
-    --rsync-path="sudo /usr/bin/rsync" \
-    --verbose --chmod=+rx --recursive --delete --force --checksum --itemize-changes \
-    build/ frontend@10.17.50.150:/var/www/html
